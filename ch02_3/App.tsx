74d9a57e1736b865338d7ee6919b7c5986bebfaa
anojds <appturbo102@gmail.com>
2022-11-19 12:15:58 +0900

    learn FuctionComponents 2

--- ch02_3/App.tsx
+++ ch02_3/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import ClassComponents from './src/screens/ClassComponents'
 import ArrowComponents from './src/screens/ArrowComponents'
+import Person from './src/screens/Person'
+import * as D from './src/data'
 
+const person = D.createRandomPerson()
 export default function App() {
     return (
     <SafeAreaView>
         <ClassComponents/>
         <ArrowComponents/>
+        <Person person={person}/>
     </SafeAreaView>
     )
 }
